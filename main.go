1b067f83e34b29b15ed21cb49c9c2f544fd34256
Santhosh <santhosh@spectrocloud.com>
2022-10-28 19:58:03 +0530

    update proxy env and restart services

--- main.go
+++ main.go
@@ -64,28 +64,15 @@ func clusterProvider(cluster clusterplugin.Cluster) yip.YipConfig {
 	cfg := yip.YipConfig{
 		Name: "RKE2 C3OS Cluster Provider",
 		Stages: map[string][]yip.Stage{
-			"boot.before": {
+			"boot.after": {
 				{
-					Name: "Install RKE2 Configuration Files",
+					Name: "",
 					Files: []yip.File{
-						{
-							Path:        filepath.Join(configurationPath, "90_userdata.yaml"),
-							Permissions: 0400,
-							Content:     string(userOptions),
-						},
-						{
-							Path:        filepath.Join(configurationPath, "99_userdata.yaml"),
-							Permissions: 0400,
-							Content:     string(options),
-						},
 						{
 							Path:        filepath.Join(containerdEnvConfigPath, systemName),
 							Permissions: 0400,
 							Content:     containerdProxyEnv(),
 						},
-					},
-					Commands: []string{
-						fmt.Sprintf("jq -s 'def flatten: reduce .[] as $i([]; if $i | type == \"array\" then . + ($i | flatten) else . + [$i] end); [.[] | to_entries] | flatten | reduce .[] as $dot ({}; .[$dot.key] += $dot.value)' %s/*.yaml > /etc/rancher/rke2/config.yaml", configurationPath),
 					},
 				},
 				{
@@ -100,6 +87,26 @@ func clusterProvider(cluster clusterplugin.Cluster) yip.YipConfig {
 					},
 				},
 			},
+			"boot.before": {
+				{
+					Name: " Install RKE2 Configuration Files",
+					Files: []yip.File{
+						{
+							Path:        filepath.Join(configurationPath, "90_userdata.yaml"),
+							Permissions: 0400,
+							Content:     string(userOptions),
+						},
+						{
+							Path:        filepath.Join(configurationPath, "99_userdata.yaml"),
+							Permissions: 0400,
+							Content:     string(options),
+						},
+					},
+					Commands: []string{
+						fmt.Sprintf("jq -s 'def flatten: reduce .[] as $i([]; if $i | type == \"array\" then . + ($i | flatten) else . + [$i] end); [.[] | to_entries] | flatten | reduce .[] as $dot ({}; .[$dot.key] += $dot.value)' %s/*.yaml > /etc/rancher/rke2/config.yaml", configurationPath),
+					},
+				},
+			},
 		},
 	}
 
@@ -109,14 +116,17 @@ func clusterProvider(cluster clusterplugin.Cluster) yip.YipConfig {
 func containerdProxyEnv() string {
 	var proxy []string
 	if len(os.Getenv("HTTP_PROXY")) > 0 {
+		proxy = append(proxy, "HTTP_PROXY=${HTTP_PROXY}")
 		proxy = append(proxy, "CONTAINERD_HTTP_PROXY=${HTTP_PROXY}")
 	}
 
 	if len(os.Getenv("HTTPS_PROXY")) > 0 {
+		proxy = append(proxy, "HTTPS_PROXY=${HTTPS_PROXY}")
 		proxy = append(proxy, "CONTAINERD_HTTPS_PROXY=${HTTPS_PROXY}")
 	}
 
 	if len(os.Getenv("NO_PROXY")) > 0 {
+		proxy = append(proxy, "NO_PROXY=${NO_PROXY}")
 		proxy = append(proxy, "CONTAINERD_NO_PROXY=${NO_PROXY}")
 	}
 
